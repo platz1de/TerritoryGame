7fa9d31c45e96caf3e24c0ad3ce0bd6846d2725a
platz1de <51201131+platz1de@users.noreply.github.com>
2023-08-28 10:01:07 +0200

    Add boundaries when resizing / dragging map, increase responsiveness

--- src/game/BasicMapNavigationHandler.ts
+++ src/game/BasicMapNavigationHandler.ts
@@ -2,7 +2,7 @@ import {interactionRegistry} from "../interaction/InteractionManager";
 import {DragInteractionHandler, ScrollInteractionHandler} from "../interaction/InteractionHandlerTypes";
 import {AxialCoordinate} from "../math/AxialCoordinate";
 import {gameMapRendererManager} from "./GameManager";
-import {eventManager, gameManager} from "../main";
+import {eventManager, gameManager, renderer} from "../main";
 
 export class BasicMapNavigationHandler implements ScrollInteractionHandler, DragInteractionHandler {
 	x: number = 0;
@@ -12,10 +12,12 @@ export class BasicMapNavigationHandler implements ScrollInteractionHandler, Drag
 	dragY: number = 0;
 
 	enable() {
-		this.x = gameMapRendererManager.container.x;
-		this.y = gameMapRendererManager.container.y;
-		console.log(this.x, this.y);
-		this.zoom = 1;
+		let minXZoom = renderer.app.view.width / gameMapRendererManager.container.width, minYZoom = renderer.app.view.height / gameMapRendererManager.container.height;
+		this.zoom = 0.9 * Math.min(minXZoom, minYZoom);
+		gameMapRendererManager.container.scale.set(this.zoom, this.zoom);
+		this.x = (renderer.app.view.width - gameMapRendererManager.container.width) / 2;
+		this.y = (renderer.app.view.height - gameMapRendererManager.container.height) / 2;
+		gameMapRendererManager.container.position.set(this.x + Math.sqrt(3) * this.zoom, this.y + 2 * this.zoom);
 		interactionRegistry.registerDragHandler(this);
 		interactionRegistry.registerScrollHandler(this);
 	}
@@ -27,20 +29,22 @@ export class BasicMapNavigationHandler implements ScrollInteractionHandler, Drag
 
 	onScroll(x: number, y: number, delta: number): void {
 		let mapX = this.getMapX(x), mapY = this.getMapY(y);
-		this.zoom -= Math.max(-1, Math.min(1, delta)) * 0.1 * this.zoom;
-		this.x = -mapX * this.zoom + x;
-		this.y = -mapY * this.zoom + y;
-		gameMapRendererManager.container.setTransform(this.x, this.y, this.zoom, this.zoom);
+		this.zoom -= Math.max(-1, Math.min(1, delta)) * 0.3 * this.zoom;
+		this.zoom = Math.max(0.5, Math.min(1000, this.zoom));
+		gameMapRendererManager.container.scale.set(this.zoom, this.zoom);
+		this.x = Math.max(Math.min(-mapX * this.zoom + x, 9 * renderer.app.view.width / 10), -gameMapRendererManager.container.width + renderer.app.view.width / 10);
+		this.y = Math.max(Math.min(-mapY * this.zoom + y, 9 * renderer.app.view.height / 10), -gameMapRendererManager.container.height + renderer.app.view.height / 10);
+		gameMapRendererManager.container.position.set(this.x + Math.sqrt(3) * this.zoom, this.y + 2 * this.zoom);
 		eventManager.onMapMove();
 		eventManager.onScroll();
 	}
 
 	testScroll(x: number, y: number): boolean {
-		return this.isInMap(x, y);
+		return true;
 	}
 
 	testDrag(x: number, y: number): boolean {
-		return this.isInMap(x, y);
+		return true;
 	}
 
 	onDragStart(x: number, y: number): void {
@@ -55,11 +59,11 @@ export class BasicMapNavigationHandler implements ScrollInteractionHandler, Drag
 	}
 
 	onDragMove(x: number, y: number): void {
-		this.x += x - this.dragX;
-		this.y += y - this.dragY;
+		this.x = Math.max(Math.min(this.x + x - this.dragX, 9 * renderer.app.view.width / 10), -gameMapRendererManager.container.width + renderer.app.view.width / 10);
+		this.y = Math.max(Math.min(this.y + y - this.dragY, 9 * renderer.app.view.height / 10), -gameMapRendererManager.container.height + renderer.app.view.height / 10);
 		this.dragX = x;
 		this.dragY = y;
-		gameMapRendererManager.container.setTransform(this.x, this.y, this.zoom, this.zoom);
+		gameMapRendererManager.container.position.set(this.x + Math.sqrt(3) * this.zoom, this.y + 2 * this.zoom);
 		eventManager.onMapMove();
 	}
 
